a30fcb8f65643f9c1775777bf15d75c862891ba0
Joshua "jag" Ginsberg <jag@flowtheory.net>
2020-09-12 16:31:02 -0400

    Add ECS core fields.

--- logging/emit.go
+++ logging/emit.go
@@ -3,7 +3,31 @@ package logging
 import (
 	"encoding/json"
 	"log"
+	"time"
+	"os"
 )
+
+var tags []string
+var labels map[string]string
+
+func init() {
+	tagsFromEnv, exists := os.LookupEnv("HABERDASHER_TAGS")
+	if !exists {
+		tagsFromEnv = "[]"
+	}
+	labelsFromEnv, exists := os.LookupEnv("HABERDASHER_LABELS")
+	if !exists {
+		labelsFromEnv = "{}"
+	}
+	err := json.Unmarshal([]byte(tagsFromEnv), &tags)
+	if err != nil {
+		log.Fatal("HABERDASHER_TAGS must be a JSON array of strings")
+	}
+	err = json.Unmarshal([]byte(labelsFromEnv), &labels)
+	if err != nil {
+		log.Fatal("HABERDASHER_LABELS must be a JSON object of strings")
+	}
+}
 
 // An Emitter defines how to ship a log message to a log service.
 type Emitter interface {
@@ -12,6 +36,9 @@ type Emitter interface {
 
 // A Message is a structured log message
 type Message struct {
+	Timestamp time.Time `json:"@timestamp"`
+	Labels map[string]string `json:"labels"`
+	Tags []string `json:"tags"`
 	Message string `json:"message"`
 }
 
@@ -26,7 +53,7 @@ func Register(emitterType string, emitter Emitter) {
 // Emit is launched as a goroutine for individual log lines to be sent
 // concurrently.
 func Emit(emitter Emitter, logMessage string) {
-	m := Message{logMessage}
+	m := Message{time.Now(), labels, tags, logMessage}
 	jsonBytes, _ := json.Marshal(m)
 	if err := emitter.HandleLogMessage(jsonBytes); err != nil {
 		log.Println("Error emitting message:", jsonBytes, err)
