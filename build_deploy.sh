8a7b5f1fad4422cf5f2c3c7feaaa12c291e19431
Joshua "jag" Ginsberg <jag@flowtheory.net>
2020-09-29 16:46:44 -0400

    Setup goreleaser for CI.

---
+++ build_deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+set -exv
+
+GORELEASER_URL="https://github.com/goreleaser/goreleaser/releases"
+
+test -z "$TMPDIR" && TMPDIR="$(mktemp -d)"
+
+last_version() {
+  curl -sL -o /dev/null -w %{url_effective} "$GORELEASER_URL/latest" | 
+    rev | 
+    cut -f1 -d'/'| 
+    rev
+}
+
+test -z "$VERSION" && VERSION="$(last_version)"
+test -z "$VERSION" && {
+echo "Unable to get goreleaser version." >&2
+exit 1
+}
+rm -f "/tmp/goreleaser.tgz"
+curl -L -o "/tmp/goreleaser.tgz" "$GORELEASER_URL/download/$VERSION/goreleaser_$(uname -s)_$(uname -m).tar.gz"
+
+tar -xf "/tmp/goreleaser.tgz" -C "$TMPDIR"
+GITHUB_TOKEN=$APP_SRE_BOT_PUSH_TOKEN "${TMPDIR}/goreleaser" "$@"
