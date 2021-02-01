ed9edfcff88e2c5efd32d84e67241b93bd36cb56
Pete Savage <psavage@redhat.com>
2021-02-01 10:40:17 +0000

    Added clowder support

--- go.mod
+++ go.mod
@@ -4,5 +4,6 @@ go 1.14
 
 require (
 	github.com/ramr/go-reaper v0.2.0
+	github.com/redhatinsights/app-common-go v0.0.1-pre-log
 	github.com/segmentio/kafka-go v0.4.2
 )
--- logging/emit.go
+++ logging/emit.go
@@ -3,12 +3,15 @@ package logging
 import (
 	"encoding/json"
 	"log"
-	"time"
 	"os"
+	"time"
+
+	clowder "github.com/redhatinsights/app-common-go/pkg/api/v1"
 )
 
 var defaultTags []string
 var defaultLabels map[string]string
+
 const defaultEcsVersion = "1.5.0"
 
 // If the wrapped application emits plain text messages, we should wrap them
@@ -16,39 +19,47 @@ const defaultEcsVersion = "1.5.0"
 // HABERDASHER_TAGS and HABERDASHER_LABELS contain serialized JSON values for
 // the tags and labels to go in such messages. They are optional.
 func init() {
-	tagsFromEnv, exists := os.LookupEnv("HABERDASHER_TAGS")
-	if !exists || tagsFromEnv == "" {
-		tagsFromEnv = "[]"
-	}
-	labelsFromEnv, exists := os.LookupEnv("HABERDASHER_LABELS")
-	if !exists || labelsFromEnv == "" {
-		labelsFromEnv = "{}"
-	}
-	err := json.Unmarshal([]byte(tagsFromEnv), &defaultTags)
-	if err != nil {
-		log.Fatal("HABERDASHER_TAGS must be a JSON array of strings")
-	}
-	err = json.Unmarshal([]byte(labelsFromEnv), &defaultLabels)
-	if err != nil {
-		log.Fatal("HABERDASHER_LABELS must be a JSON object of strings")
+	if clowder.IsClowderEnabled() {
+		defaultTags = clowder.LoadedConfig.Logging.Tags
+		for _, label := range clowder.LoadedConfig.Logging.Labels {
+			defaultLabels[label.Name] = label.Value
+		}
+	} else {
+		tagsFromEnv, exists := os.LookupEnv("HABERDASHER_TAGS")
+		if !exists || tagsFromEnv == "" {
+			tagsFromEnv = "[]"
+		}
+		labelsFromEnv, exists := os.LookupEnv("HABERDASHER_LABELS")
+		if !exists || labelsFromEnv == "" {
+			labelsFromEnv = "{}"
+		}
+
+		err := json.Unmarshal([]byte(tagsFromEnv), &defaultTags)
+		if err != nil {
+			log.Fatal("HABERDASHER_TAGS must be a JSON array of strings")
+		}
+		err = json.Unmarshal([]byte(labelsFromEnv), &defaultLabels)
+		if err != nil {
+			log.Fatal("HABERDASHER_LABELS must be a JSON object of strings")
+		}
 	}
 }
 
 // An Emitter defines how to ship a log message to a log service.
 type Emitter interface {
 	Setup()
-	HandleLogMessage(jsonSerializeable interface{}) (error)
-	Cleanup() (error)
+	HandleLogMessage(jsonSerializeable interface{}) error
+	Cleanup() error
 }
 
 // A Message is a structured log message - only used if the log message we
 // consume from the subprocess is not already structured
 type Message struct {
-	ECSVersion string `json:"ecs.version"`
-	Timestamp time.Time `json:"@timestamp"`
-	Labels map[string]string `json:"labels"`
-	Tags []string `json:"tags"`
-	Message string `json:"message"`
+	ECSVersion string            `json:"ecs.version"`
+	Timestamp  time.Time         `json:"@timestamp"`
+	Labels     map[string]string `json:"labels"`
+	Tags       []string          `json:"tags"`
+	Message    string            `json:"message"`
 }
 
 // Emitters is the registry of Emitter implementers
